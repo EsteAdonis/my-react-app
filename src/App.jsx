3c63472129771297cb1cc28b05832f5a88c2b3df
EsteAdonis 2025 <julio.e.ochoa.p@gmail.com>
2024-06-07 22:00:58 -0600

    Button with external css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import Card from "./Card/Card"
+// How to style react components with css
+// --
+// (not including external frameworks or preprocessors)
+
+// 1. External    <-- Working with external css
+
+import Button from "./Button/Button"
 
 function App() {
   return (
     <>
-      <Card />
-      <Card />
-      <Card />      
+    <Button/>
     </>
   )
 }
