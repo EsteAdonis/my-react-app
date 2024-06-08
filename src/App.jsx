c614d05c67317380ac440161a4ea6c67732141ae
EsteAdonis 2025 <julio.e.ochoa.p@gmail.com>
2024-06-08 12:42:59 -0600

    Rendering List

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
-// conditional rendering = allows you to control what gets rendered
-//                         in your application based on certain conditions
-//                         (show, hide, or change components)
-
-import UserGreeting from "./UserGreetings/UserGreeting"
-
+import List from "./List/List"
 function App() {
   return (
     <>
-      <UserGreeting isLoggedIn={true} username="Adonis" />
-      <UserGreeting />      
+      <List />
     </>
   )
 }
