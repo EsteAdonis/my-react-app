96273b58aec7b0f89243bd213611a9db606491c8
EsteAdonis 2025 <julio.e.ochoa.p@gmail.com>
2024-06-10 13:03:40 -0600

    useState Hook - Counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-// click event = An interaction when a user clicks on a specific element.
-//               We can respond to clicks by passing
-//               a callback to the onClick event handler.
+// React hook = Special function that allows functional components
+//              to use React features without writing class components (React, v16.8)
+//              (useState, useEffect, useContext, useReducer, useCallBack, and more... )
 
-// 
+import Counter from "./Counter/Counter"
 
-import Button from "./ClickEvents-Button/Button"
-import ProfilePicture from "./ProfilePicture/ProfilePicture.jsx"
+// useState() = A React hook that allows the creation of a stateful variable
+//              AND a setter function to update its value in the Virtual DOM.
+//              [name, setName]
+
 
 function App() {
   
   return (
     <>
-      <ProfilePicture></ProfilePicture>
-      <Button />
+      <Counter />
     </>
   )
 }
