71d7b6d844d70523287e677140afaf558b4e2315
EsteAdonis 2025 <julio.e.ochoa.p@gmail.com>
2024-06-15 23:02:55 -0600

    UseRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import ComponentA from "./UseContext/ComponentA"
-  
+// useState() = Re-renders the component when the state value changes.
+
+// useRef() = "use Reference" Does not cause not cause re-renders  when its value changes.
+//             when you want a component to "remember" some information
+//             but you don't want that information to trigger new renders.
+
+//             1. Accessing/Interacting with DOM elements
+//             2. Handling Focus, Animations, and Transitions
+//             3. Managing Timers and Intervals
+
 function App() {
   
   return (
     <>
-      <ComponentA />
+
     </>
   )
 }
