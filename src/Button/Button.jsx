de6212cb10fe95ac5390fc2e3e79ce339d2e1897
EsteAdonis 2025 <julio.e.ochoa.p@gmail.com>
2024-07-05 22:03:14 -0600

    Adding Storybook

--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 function Button() {
 
   const styles = {
@@ -14,4 +16,12 @@ function Button() {
   )
 }
 
+Button.propTypes = {
+  label: PropTypes.string,
+  backgroundColor: PropTypes.string,
+  size: PropTypes.oneOf(['sm', 'md', 'lg']),
+  onclick: PropTypes.func
+}
+
 export default Button
+
