72525c496aca3dc6f6295bb36abf0d7c7e0378b3
EsteAdonis 2025 <julio.e.ochoa.p@gmail.com>
2024-07-13 19:37:16 -0600

    Input component with onChange Event

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import Input from './Input/Input.jsx';
 import Stopwatch from './Stopwatch/Stopwatch.jsx';
 
 function App() {
   
   return (
     <>
-      <Stopwatch />
+      {/* <Stopwatch /> */}
+      <Input />
     </>
   )
 }
