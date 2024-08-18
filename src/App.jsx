596d1cb8027512b4bdacf97a886445e709a1948d
EsteAdonis 2025 <julio.e.ochoa.p@gmail.com>
2024-08-18 17:44:15 -0600

    Working with Textarea & Initializando Github Actions

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import ChangeBackgroundColor from './Change-Background-Color/Change-Background-Color.jsx';
 import Input from './Input/Input.jsx';
 import Stopwatch from './Stopwatch/Stopwatch.jsx';
+import Textarea from './Textarea/Textarea.tsx';
+
+
 
 function App() {
+  function handleInput(e) {
+    console.log(e);
+  }
   
   return (
     <>
       {/* <Stopwatch /> */}
-      <Input />
-      <ChangeBackgroundColor />
+      <Textarea
+        showPlaceholder={false}
+        textPlaceholder='Greetings Julio Esteban'
+        readOnly={false}
+        error={false}
+        value={handleInput}
+      />
+      {/* <Input />
+      <ChangeBackgroundColor /> */}
     </>
   )
 }
